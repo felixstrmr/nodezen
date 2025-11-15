5a249e4391579c29e5944ef66d793fbf716cfed1
strmfelix <email@felixstrohmeier.com>
2025-11-15 21:56:50 +0100

    feat: add @vercel/analytics and @vercel/speed-insights dependencies

--- components/providers/root-provider.tsx
+++ components/providers/root-provider.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import {
   CircleCheckIcon,
   CircleXIcon,
@@ -25,6 +27,8 @@ export default function RootProvider(props: { children: React.ReactNode }) {
             close: <XIcon className="size-4 text-zinc-500" />,
           }}
         />
+        <Analytics />
+        <SpeedInsights />
       </ThemeProvider>
     </NuqsAdapter>
   );
