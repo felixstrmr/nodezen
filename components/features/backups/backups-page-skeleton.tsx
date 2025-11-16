65ca12c8f929588335f5603ec1f92b48c3d1acb3
strmfelix <email@felixstrohmeier.com>
2025-11-16 12:16:39 +0100

    feat: update backups page skeleton to include header for backup details and maintain loading spinner alignment

--- components/features/backups/backups-page-skeleton.tsx
+++ components/features/backups/backups-page-skeleton.tsx
@@ -2,8 +2,15 @@ import { Spinner } from "@/components/ui/spinner";
 
 export default function BackupsPageSkeleton() {
   return (
-    <div className="flex size-full items-center justify-center rounded-lg border">
-      <Spinner />
+    <div className="flex size-full flex-col rounded-lg border">
+      <div className="grid grid-cols-[1fr_15rem_15rem] rounded-t-lg bg-accent/30 p-3">
+        <p>Name</p>
+        <p>Size</p>
+        <p>Created</p>
+      </div>
+      <div className="flex size-full items-center justify-center">
+        <Spinner />
+      </div>
     </div>
   );
 }
