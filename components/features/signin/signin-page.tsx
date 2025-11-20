5d51e3ce023d21c90c8e41fb9d218f6ec04245a6
Felix Strohmeier <email@felixstrohmeier.com>
2025-11-20 13:28:04 +0100

    feat: add logo and text to signin page

--- components/features/signin/signin-page.tsx
+++ components/features/signin/signin-page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 import SigninForm from "@/components/forms/signin-form";
+import NodezenIcon from "@/components/icons/nodezen-icon";
 import { getUser } from "@/queries/user";
 import { getWorkspace } from "@/queries/workspace";
 
@@ -20,7 +21,18 @@ export default async function SigninPage() {
 
   return (
     <div className="flex size-full items-center justify-center">
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col">
+        <div className="flex size-8 items-center justify-center rounded-md bg-primary">
+          <NodezenIcon className="size-4 text-primary-foreground" />
+        </div>
+        <div className="mt-4 mb-8">
+          <h1 className="font-semibold text-2xl tracking-tight">
+            Welcome back!
+          </h1>
+          <p className="text-muted-foreground text-sm">
+            Enter your details to continue.
+          </p>
+        </div>
         <SigninForm />
       </div>
     </div>
